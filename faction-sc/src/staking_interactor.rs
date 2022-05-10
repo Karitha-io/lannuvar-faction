aa0799f3308a9ee1f49ad0772a6d89aa2da9aee6
vipcookie90 <vip.cookie90@protonmail.com>
2022-05-10 17:46:09 +0300

    added only_owner guards

--- faction-sc/src/staking_interactor.rs
+++ faction-sc/src/staking_interactor.rs
@@ -30,6 +30,7 @@ pub trait StakingIntModule {
     #[proxy]
     fn staking_poxy(&self, to: ManagedAddress) -> staking_poxy::Proxy<Self::Api>;
 
+    #[only_owner]
     #[endpoint(stakeGenesisNft)]
     fn stake_genesis_nft(&self, nonce: u64) {
         let bussiness_token_id = TokenIdentifier::from_esdt_bytes(GENESIS_NFT_ID);
@@ -43,6 +44,7 @@ pub trait StakingIntModule {
             .execute_on_dest_context_ignore_result();
     }
 
+    #[only_owner]
     #[endpoint(unstakeGenesisNft)]
     fn unstake_genesis_nft(&self, nonce: u16) {
         let bussiness_token_id = TokenIdentifier::from_esdt_bytes(GENESIS_NFT_ID);
@@ -58,6 +60,7 @@ pub trait StakingIntModule {
         require!(balance == BigUint::from(1u64), "did not receive nft");
     }
 
+    #[only_owner]
     #[endpoint(claimRewardsGenesis)]
     fn claim_rewards_genesis(&self, nonce: u16) {
         self.staking_poxy(self.staking_address().get())
